d5913ca620eca2759345513b14fe35577f6adb29
lordruzki <ruizramos04@outlook.com>
2023-11-15 22:26:40 -0500

    Basic testing with some buttons

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('shaun');
+  const [person, setPerson] = useState({ name: 'mario', age: 40 })
+
+  const clickHandlerOne = () => {
+    setName('shaun');
+  }
+  const clickHandlerTwo = () => {
+    setName('chun-li');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Cookies here!</Text>
-      <StatusBar style="auto" />
+      <Text>My name is {name}.</Text>
+      <View style={styles.buttonContainer}>
+        <Button title='update chun-li' onPress={clickHandlerOne}/>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title='update shaun' onPress={clickHandlerTwo}/>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles= StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  buttonContainer: {
+    marginTop: 50,
   },
 });
 
